e552509bb8739c1b48ff12790a3a7109f58f56e4
Gor <7495500@gmail.com>
2020-05-06 13:47:59 +0300

    Display products

---
+++ public/app.js
@@ -0,0 +1,7 @@
+function something() {
+  var x = window.localStorage.getItem('bbb'); // x = hh['bbb']
+  x = x * 1 + 1; // x += 1
+
+  window.localStorage.setItem('bbb', x); // hh['bbb'] = x
+  alert(x);
+}
